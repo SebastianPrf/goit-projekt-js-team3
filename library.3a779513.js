415334be3246930583acebd59c018563db177f32
SebastianPrf <107537612+SebastianPrf@users.noreply.github.com>
2023-07-25 19:16:10 +0000

    Deploying to gh-pages from @ SebastianPrf/goit-projekt-js-team3@5e2e4d2a748cb2018ad0abc1a7f85515f31d2d9a 🚀

--- library.3a779513.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};e.closeModalBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")}))})();
-//# sourceMappingURL=library.3a779513.js.map
